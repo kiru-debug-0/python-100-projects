4e6d271949d2217c122da181386607f98f78a030
Sanskriti papneja <kritipapneja@gmail.com>
2026-09-01 17:41:09 +0530

    Create 07_temp_change.py

---
+++ 01_basics/07_temp_change.py
@@ -0,0 +1,4 @@
+# to change the temperature from celcius to fahrenheit
+temp=int(input("enter temperature in celcius"))
+chtemp=(9/5)*temp + 32
+print(" changed temperature in fahrenheit = ",chtemp)
