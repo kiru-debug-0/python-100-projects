370c2c2b18530a51d931ce82932a31b138b79503
Sanskriti papneja <kritipapneja@gmail.com>
2026-09-01 17:36:46 +0530

    Implement area and circumference calculation for circle
    Add a script to calculate area and circumference of a circle.

---
+++ 01_basics/06_area_circum_circle.py
@@ -0,0 +1,6 @@
+# to calculate area and circumference of circle 
+r=int(input("enter radius"))
+area=3.14*r*r
+cir=2*3.14*r
+print(" area of circle= ",area)
+print(" circumference of circle = ",cir)
