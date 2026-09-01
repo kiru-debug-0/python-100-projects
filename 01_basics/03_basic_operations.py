b19b458eb9d37ec809785276966de5f7b9e92e8c
Sanskriti papneja <kritipapneja@gmail.com>
2026-09-01 17:17:00 +0530

    Create 03_basic_operations.py

---
+++ 01_basics/03_basic_operations.py
@@ -0,0 +1,13 @@
+# program to perform different operation on 2 inputs from user 
+a=int(input("enter number 1 "))
+b= int (input ( " enter number 2 "))
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+rem=a%b
+print(" addition of numbers ",add)
+print(" substraction of numbers",sub)
+print(" multiplication of numbers",mul)
+print(" division of numbers ",div)
+print(" remainder of division ",rem )
