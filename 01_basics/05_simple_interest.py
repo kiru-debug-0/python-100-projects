8b3d778f4cd4f3ec96ed746f4fbef39f0b7e0d6e
Sanskriti papneja <kritipapneja@gmail.com>
2026-09-01 17:32:45 +0530

    Add simple interest calculation script

---
+++ 01_basics/05_simple_interest.py
@@ -0,0 +1,6 @@
+# to calculate simple interest 
+p=int(input("enter principal amount "))
+r=float(input("enter rate "))
+t=int(input("enter time"))
+si=(p*r*t)/100
+print("Simple Interest of given data =",si)
