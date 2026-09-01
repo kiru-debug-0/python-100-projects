f5847292be7bc9d348981df9d7f37719cc7afdd2
Sanskriti papneja <kritipapneja@gmail.com>
2026-09-01 17:25:32 +0530

    Add value swapping program in sorting_values.py

---
+++ 01_basics/04_sorting_values.py
@@ -0,0 +1,9 @@
+# program to swap the values given by user 
+a=int(input(" enter value 1 "))
+b=int(input(" enter value 2 "))
+print("swapping starts ")
+c=a
+a=b
+b=c
+print(" now the value of 1st operator = ",a)
+print(" and the value of 2nd operator =",b)
