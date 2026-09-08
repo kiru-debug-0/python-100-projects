6dda3a7ada4136e6b3e65ae951a6590558c48cd0
Sanskriti papneja <kritipapneja@gmail.com>
2026-09-08 21:42:15 +0530

    Add program to find the largest of two numbers

---
+++ 01_basics/10_large_two.py
@@ -0,0 +1,8 @@
+a=int(input(" enter 1st number "))
+b=int(input(" enter 2nd number "))
+if a>b:
+  print(" largest number = ",a)
+elif b>a:
+  print(" largest number = ",b)
+else:
+  print(" both number are equal ")
